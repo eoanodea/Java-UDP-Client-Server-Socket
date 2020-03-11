b5e5117bb6e77ab32063f962f6aeec09a60ae1aa
Eoan O Dea <=>
2020-03-11 15:33:53 +0000

    Created a UDP client and server which can communicate between eachother. The client takes in a input string which is sent to the server through a DatagramSocket. The server converts the string to uppercase, and returns it to the client

---
+++ UDPClient/src/udpclient/UDPClient.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udpclient;
+
+import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author n00162393
+ */
+public class UDPClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception{
+        String sentence;
+        String modifiedSentence;
+        
+        Scanner inFromUser = new Scanner(System.in);
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+        
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+        
+        System.out.println("Please enter a message to send to the server:");
+        sentence = inFromUser.nextLine();
+        sendData =sentence.getBytes();
+        
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+        clientSocket.send(sendPacket);
+        
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+        
+        modifiedSentence = new String(receivePacket.getData());
+        System.out.println("From Server: " + modifiedSentence);
+        clientSocket.close();
+    }
+    
+}
